f7353924f4a8ddf073884859db9ac21985ed99a7
rohithraju07 <137498650+rohithraju07@users.noreply.github.com>
2023-07-26 12:31:06 +0530

    Add files via upload

---
+++ cart-ms/src/main/java/com/mysmartshop/cart/api/cartController.java
@@ -0,0 +1,67 @@
+package com.mysmartshop.cart.api;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.mysmartshop.cart.Service.CartService;
+import com.mysmartshop.cart.dto.CartDetails;
+import com.mysmartshop.cart.model.CartItem;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@RestController
+@RequestMapping("/api/cart")
+public class cartController {
+	
+	@Autowired
+	private CartService cartService;
+	
+	
+	@GetMapping
+	public String greet() {
+		return "Message from Cart Service";
+	}
+	
+	@GetMapping("/items")
+	public CartDetails getCartDetails() {
+		List<CartItem> items = cartService.getAllItems();
+		float totalCartVlue = cartService.calculateTotalCost();
+		CartDetails cartDetails = new CartDetails();
+		cartDetails.setItemsList(items);
+		cartDetails.setTotalCartValue(totalCartVlue);
+		return cartDetails;
+		
+	}
+	
+	@PostMapping("/items/product/{productId}")
+	@CircuitBreaker(fallbackMethod = "addNewItemFallback", name = "cb-product")
+	public List<CartItem> addNewItem(@PathVariable String productId){
+		return cartService.addToCart(productId);
+	}
+	
+	
+	public List<CartItem> addNewItemFallback(@PathVariable String productId, Throwable t){
+		System.err.println(t.getMessage());
+		return new ArrayList<CartItem>();
+	}
+	
+
+	
+	@DeleteMapping("/items/product/{productId}")
+	public List<CartItem> deleteItem(@PathVariable String productId){
+		return cartService.removeFromCart(productId);
+	}
+
+
+
+}
---
+++ cart-ms/src/main/java/com/mysmartshop/cart/dto/Product.java
@@ -0,0 +1,13 @@
+package com.mysmartshop.cart.dto;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+	private String productId;
+	private String name;
+	private float price;
+	private String description;
+
+}
